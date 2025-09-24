e21d082c6c3252cd8c6dbac010c677141f4cae67
Zuluaga17 <zuluaga110922@gmail.com>
2025-09-24 15:53:44 -0500

    centrar columnas y añadir espaciado en StartOrderScreen

--- app/src/main/java/com/example/cupcakeapp/ui/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/ui/StartOrderScreen.kt
@@ -6,6 +6,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cupcakeapp.ui.theme.CupcakeAppTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.dimensionResource
+import com.example.cupcakeapp.R
+
+
 
 @Composable
 fun StartOrderScreen(
@@ -15,7 +21,26 @@ fun StartOrderScreen(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        // Aquí irán los elementos de la pantalla
+        @Composable
+        fun StartOrderScreen(
+            modifier: Modifier = Modifier
+        ) {
+            Column(
+                modifier = modifier,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(
+                        dimensionResource(R.dimen.padding_small)
+                    )
+                ) {
+                    // Aquí irán los elementos de la parte superior
+                }
+            }
+        }
+
     }
 }
 @Preview
