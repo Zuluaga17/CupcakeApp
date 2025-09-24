2b44b569fb351d8ec1cffc8bb5cd52391550048e
Zuluaga17 <zuluaga110922@gmail.com>
2025-09-24 16:17:07 -0500

    agregar texto de título de pedido con espaciado

--- app/src/main/java/com/example/cupcakeapp/ui/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/ui/StartOrderScreen.kt
@@ -16,6 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.res.stringResource
 
 
 
@@ -56,6 +59,12 @@ fun StartOrderScreen(
                         )
 
                         Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+                        Text(
+                            text = stringResource(R.string.order_cupcakes),
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+
                     }
 
                 }
